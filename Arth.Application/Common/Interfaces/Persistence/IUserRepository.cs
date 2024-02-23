c39e55f6cfa551367558566b8c83c2690aa81a57
Daedaluzz <arthurbernardpereira@gmail.com>
2024-02-23 11:19:59 -0300

    Feat: Creation of user interface on application layer and user entity on domain

---
+++ Arth.Application/Common/Interfaces/Persistence/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Arth.Domain.Entities;
+
+namespace Arth.Application.Common.Interfaces.Persistence;
+
+public interface IUserRepository
+{
+    User? GetUserByEmail(string email);
+    void Add(User user);
+}
--- Arth.Application/Services/Authentication/AuthenticationService.cs
+++ Arth.Application/Services/Authentication/AuthenticationService.cs
@@ -1,29 +1,46 @@
 ﻿
 using Arth.Application.Common.Interfaces.Authentication;
+using Arth.Application.Common.Interfaces.Persistence;
+using Arth.Domain.Entities;
 
 namespace Arth.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
     private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
 
-    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
     {
         _jwtTokenGenerator = jwtTokenGenerator;
+        _userRepository = userRepository;
     }
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
-        //Check if the user alredy exists
+        //Validating if the user dosn't exist
+        if (_userRepository.GetUserByEmail(email) is not null)
+        {
+            throw new Exception("user with given email alredy exists");
+        }
 
-        //Create user (generate unique ID)
+
+        //Create user (generate unique ID) & persist to the DB
+        var user = new User
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            Email = email,
+            Password = password
+        };
+
+        _userRepository.Add(user);
 
         //Create JWT token
-        Guid userId = Guid.NewGuid();
-        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+        var token = _jwtTokenGenerator.GenerateToken(user.Id, firstName, lastName);
 
         return new AuthenticationResult(
-            userId,
+            user.Id,
             firstName,
             lastName,
             email,
@@ -32,6 +49,11 @@ public class AuthenticationService : IAuthenticationService
 
     public AuthenticationResult Login(string email, string password)
     {
-        return new AuthenticationResult(Guid.NewGuid(), "firstName", "lastName", email, "token");
+        return new AuthenticationResult(
+            Guid.NewGuid(),
+            "firstName",
+            "lastName",
+            email,
+            "token");
     }
 }
