876cdb87920d75d8a65de229789d86e2d9dad360
Daedaluzz <arthurbernardpereira@gmail.com>
2024-02-22 23:15:49 -0300

    Build: Microsoft.IdentityModel.JsonWebTokens added to Arth.Architecture

---
+++ Arth.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,20 @@
+﻿using Arth.Application.Common.Interfaces.Authentication;
+using System.Security.Claims;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+namespace Arth.Infrastructure.Authentication;
+
+public class JwtTokenGenerator : IJwtTokenGenerator
+{
+    public string GenerateToken(Guid userId, string firstName, string lastName)
+    {
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+            new Claim(JwtRegisteredClaimNames.GivenName, firstName),
+            new Claim(JwtRegisteredClaimNames.FamilyName, lastName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+
+        };
+    }
+}
