a705fb4f24ecf0ffdb57c8b5e9eea3fb3637623e
Daedaluzz <arthurbernardpereira@gmail.com>
2024-02-24 16:38:41 -0300

    Feat: implementing entity

---
+++ Arth.Domain/Common/Models/AggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿namespace Arth.Domain.Common.Models;
+
+public abstract class AggregateRoot<TId> : Entity<TId>
+    where TId : notnull
+{
+    protected AggregateRoot(TId id) : base(id)
+    {
+
+    }
+
+}
